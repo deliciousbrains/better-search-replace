a4443e42a2bda1e24c983c7d36f3e8fdeffd84b9
Matt Shaw <matt@expandedfronts.com>
2015-11-10 14:01:32 -0500

    Use printf in BSR_Admin::load_details, fix for untranslatable string

--- includes/class-bsr-admin.php
+++ includes/class-bsr-admin.php
@@ -225,7 +225,15 @@ class BSR_Admin {
 								$report['updates'] = '<strong>' . $report['updates'] . '</strong>';
 							}
 
-							echo '<tr><td class="bsr-first">' . $table_name . '</td><td class="bsr-second">' . $report['change'] . '</td><td class="bsr-third">' . $report['updates'] . '</td><td class="bsr-fourth">' . round( $time, 3 ) . ' seconds</td></tr>';
+							printf(
+								'<tr><td class="bsr-first">%s</td><td class="bsr-second">%d</td><td class="bsr-third">%d</td><td class="bsr-fourth">%s %s</td></tr>',
+								$table_name,
+								$report['change'],
+								$report['updates'],
+								round( $time, 3 ),
+								__( 'seconds', 'better-search-replace' )
+							);
+
 						}
 					?>
 					</tbody>
