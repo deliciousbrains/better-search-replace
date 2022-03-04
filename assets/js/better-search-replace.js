ed99789e3d341831ce0b62e799e87df378b3c454
Matt Shaw <matt@expandedfronts.com>
2022-03-03 21:02:19 -0500

    Adding UI reskin

--- assets/js/better-search-replace.js
+++ assets/js/better-search-replace.js
@@ -56,7 +56,7 @@
 		}).fail(function (response) {
 			$('.bsr-processing-wrap').remove();
 			$('.bsr-disabled').removeClass('bsr-disabled button-disabled' );
-			$('#bsr-error-wrap').html( '<div class="error"><p>' + bsr_object_vars.unknown + '</p></div>' );
+			$('#bsr-error-wrap').html( '<div class="error"><p>' + bsr_object_vars.unknown + '</p></div>' ).show();
 			if ( window.console && window.console.log ) {
 				console.log(response);
 			}
@@ -78,16 +78,16 @@
 			if ( ! search_replace_submit.hasClass( 'button-disabled' ) ) {
 
 				if ( ! $( '#search_for' ).val() ) {
-					bsr_error_wrap.html( '<div class="error"><p>' + bsr_object_vars.no_search + '</p></div>' );
+					bsr_error_wrap.html( '<div class="error"><p>' + bsr_object_vars.no_search + '</p></div>' ).show();
 				} else if ( ! $( '#bsr-table-select' ).val() ) {
-					bsr_error_wrap.html( '<div class="error"><p>' + bsr_object_vars.no_tables + '</p></div>' );
+					bsr_error_wrap.html( '<div class="error"><p>' + bsr_object_vars.no_tables + '</p></div>' ).show();
 				} else {
 					var str 	= $( '.bsr-action-form' ).serialize();
 					var data 	= str.replace(/%5C/g, "#BSR_BACKSLASH#" );
 
-					bsr_error_wrap.html('');
+					bsr_error_wrap.html('').hide();
 					search_replace_submit.addClass( 'bsr-disabled button-disabled' );
-					$( '#bsr-submit-wrap' ).append('<div class="bsr-processing-wrap"><div class="spinner is-active bsr-spinner"></div><div class="bsr-progress-wrap"><div class="bsr-progress"></div></div></div>');
+					$( '#bsr-submit-wrap' ).before('<div class="bsr-processing-wrap"><div class="spinner is-active bsr-spinner"></div><div class="bsr-progress-wrap"><div class="bsr-progress"></div></div></div>');
 					$('.bsr-progress-wrap').append( '<p class="description bsr-description">' + bsr_object_vars.processing + '</p>' );
 					bsr_process_step( 'process_search_replace', 0, 0, data );
 				}
@@ -128,5 +128,48 @@
 	}
 
 	bsr_init();
+
+	$( 'body' ).on( 'mouseover', '.tooltip', function( e ) {
+		var icon = $( this );
+		var bubble = $( this ).next();
+
+		// Close any that are already open
+		$( '.helper-message' ).not( bubble ).hide();
+
+		let iconWidth = icon.width();
+
+		var position = icon.position();
+		if ( bubble.hasClass( 'left' ) ) {
+			bubble.css({
+				'left': ( position.left - bubble.width() - icon.width() - 29 ) + 'px',
+				'top': ( position.top + icon.height() / 2 - 18 ) + 'px'
+			})
+		} else if ( bubble.hasClass( 'bottom' ) ) {
+			bubble.css( {
+				'left': ( ( position.left - bubble.width() / 2 ) - 5 ) + 'px',
+				'top': ( position.top + icon.height() + 19 ) + 'px'
+			} );
+		} else {
+			bubble.css( {
+				'left': ( position.left + icon.width() + 19 ) + 'px',
+				'top': ( position.top + icon.height() / 2 - 18 ) + 'px'
+			} );
+		}
+
+		bubble.toggle();
+	} );
+
+	$( 'body' ).on( 'mouseleave', '.tooltip', function( e ) {
+		$( '.helper-message' ).hide();
+	} );
+
+	$( '.notice.inline' )
+		.appendTo('.bsr-notice-container' )
+		.css( 'display', 'block' );
+
+	setTimeout(function() {
+		$( '#setting-error-settings_updated' ).prependTo( '.inside' ).css( 'display', 'block' );
+		$( '.bsr-updated' ).prependTo( '.inside' ).css( 'display', 'block' );
+	}, 20);
 
 })( jQuery );
--- templates/bsr-help.php
+++ templates/bsr-help.php
@@ -12,20 +12,70 @@
 // Prevent direct access.
 if ( ! defined( 'BSR_PATH' ) ) exit;
 
+$bsr_docs_url    = 'https://bettersearchreplace.com/docs/';
+$bsr_support_url = 'https://wordpress.org/support/plugin/better-search-replace';
+$bsr_license_key = get_option( 'bsr_license_key' );
+
+if ( false !== $bsr_license_key ) {
+	$bsr_support_url .= '?key=' . esc_attr( $bsr_license_key );
+}
+
 ?>
 
-<h3 id="bsr-help-heading"><?php _e( 'Help & Troubleshooting', 'better-search-replace' ); ?></h3>
+<div class="inside">
 
-<p><?php printf( __( 'Free support is available on the <a href="%s">plugin support forums</a>.', 'better-search-replace' ), 'https://wordpress.org/support/plugin/better-search-replace' ); ?></p>
+	<div class="panel">
 
-<p><?php printf( __( 'For premium features and priority email support, <a href="%s" style="font-weight:bold;">upgrade to pro</a>.', 'better-search-replace' ), 'https://bettersearchreplace.com/?utm_source=insideplugin&utm_medium=web&utm_content=help-tab&utm_campaign=pro-upsell' ); ?></p>
+		<div class="panel-header">
+			<h3><?php _e( 'Help & Troubleshooting', 'better-search-replace' ); ?></h3>
+		</div>
 
-<p><?php printf( __( 'Found a bug or have a feature request? Please submit an issue on <a href="%s">GitHub</a>!', 'better-search-replace' ), 'https://github.com/deliciousbrains/better-search-replace' ); ?></p>
+		<div class="panel-content">
 
-<textarea readonly="readonly" onclick="this.focus(); this.select()" style="width:750px;height:500px;font-family:Menlo,Monaco,monospace; margin-top: 15px;" name='bsr-sysinfo'><?php echo BSR_Compatibility::get_sysinfo(); ?></textarea>
+			<div>
+				<p>
+                    <?php
+                    printf(
+                            __( 'Free support is available on the <a href="%s">plugin support forums</a>.', 'better-search-replace' ),
+                        $bsr_support_url
+                    )
+                    ?>
+                </p>
+                <p>
+                    <?php
+                    printf(
+                            __( 'For premium features and priority email support, <a href="%s" style="font-weight:bold;">upgrade to pro</a>.', 'better-search-replace' ),
+                            'https://bettersearchreplace.com/?utm_source=insideplugin&utm_medium=web&utm_content=help-tab&utm_campaign=pro-upsell'
+                    );
+                    ?>
+                </p>
+                <p>
+                    <?php
+                    printf(
+                            __( 'Found a bug or have a feature request? Please submit an issue on <a href="%s">GitHub</a>!', 'better-search-replace' ),
+                            'https://github.com/deliciousbrains/better-search-replace'
+                    );
+                    ?>
+                </p>
+			</div>
 
-<p class="submit">
-	<input type="hidden" name="action" value="bsr_download_sysinfo" />
-	<?php wp_nonce_field( 'bsr_download_sysinfo', 'bsr_sysinfo_nonce' ); ?>
-	<?php submit_button( __( 'Download System Info', 'better-search-replace' ), 'primary', 'bsr-download-sysinfo', false ); ?>
-</p>
+			<!--System Info-->
+			<div class="row">
+				<div class="input-text full-width">
+					<label><strong><?php _e( 'System Info', 'better-search-replace' ); ?></strong></label>
+					<textarea readonly="readonly" onclick="this.focus(); this.select()" name='bsr-sysinfo'><?php echo BSR_Compatibility::get_sysinfo(); ?></textarea>
+				</div>
+			</div>
+
+			<!--Submit Button-->
+			<div class="row">
+				<p class="submit">
+					<input type="hidden" name="action" value="bsr_download_sysinfo" />
+					<?php wp_nonce_field( 'bsr_download_sysinfo', 'bsr_sysinfo_nonce' ); ?>
+					<input type="submit" name="bsr-download-sysinfo" id="bsr-download-sysinfo" class="button button-secondary button-sm" value="Download System Info">
+				</p>
+			</div>
+
+	   </div>
+	</div>
+</div>
