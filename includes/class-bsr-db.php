cf2c49c3721e6c99dbb4112b5935fb9cf8b5ee34
Callum Bridgford-Whittick <callum.bridgford-whittick@wpengine.com>
2026-05-27 17:51:20 +0100

    revert: wrong branch

--- includes/class-bsr-db.php
+++ includes/class-bsr-db.php
@@ -142,18 +142,17 @@ class BSR_DB {
 	}
 
 	/**
-	 * Gets the columns in a table and its primary key column names (composite keys supported).
-	 * 
+	 * Gets the columns in a table.
 	 * @access public
 	 * @param  string $table The table to check.
-	 * @return array First element: list of primary key column names (DESCRIBE order). Second: all column names.
+	 * @return array
 	 */
 	public function get_columns( $table ) {
-		$primary_keys 	= null;
+		$primary_key 	= null;
 		$columns 		= array();
 
 		if ( false === $this->table_exists( $table ) ) {
-			return array( $primary_keys, $columns );
+			return array( $primary_key, $columns );
 		}
 
 		$fields  		= $this->wpdb->get_results( 'DESCRIBE ' . $table );
@@ -161,13 +160,13 @@ class BSR_DB {
 		if ( is_array( $fields ) ) {
 			foreach ( $fields as $column ) {
 				$columns[] = $column->Field;
-				if ( 'PRI' == $column->Key ) {
-					$primary_keys[] = $column->Field;
+				if ( $column->Key == 'PRI' ) {
+					$primary_key = $column->Field;
 				}
 			}
 		}
 
-		return array( $primary_keys, $columns );
+		return array( $primary_key, $columns );
 	}
 
 	/**
@@ -205,10 +204,10 @@ class BSR_DB {
 		);
 
 		// Get a list of columns in this table.
-		list( $primary_keys, $columns ) = $this->get_columns( $table );
+		list( $primary_key, $columns ) = $this->get_columns( $table );
 
 		// Bail out early if there isn't a primary key.
-		if ( empty( $primary_keys ) ) {
+		if ( null === $primary_key ) {
 			$table_report['skipped'] = true;
 			return array( 'table_complete' => true, 'table_report' => $table_report );
 		}
@@ -231,8 +230,8 @@ class BSR_DB {
 
 				$data_to_fix = $row[ $column ];
 
-				if ( in_array( $column, $primary_keys, true ) ) {
-					$where_sql[] = $column . ' = "' . $this->mysql_escape_mimic( $data_to_fix ) . '"';
+				if ( $column == $primary_key ) {
+					$where_sql[] = $column . ' = "' .  $this->mysql_escape_mimic( $data_to_fix ) . '"';
 					continue;
 				}
 
